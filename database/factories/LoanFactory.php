7f94f06d17a28084faaa1408e8c19e94ed163cf7
Sven Wegner <sven.wegner89sn@gmail.com>
2023-07-15 20:18:04 +0200

    tests: added LoanService tests

--- database/factories/LoanFactory.php
+++ database/factories/LoanFactory.php
@@ -22,7 +22,15 @@ class LoanFactory extends Factory
     public function definition(): array
     {
         return [
-            // TODO: Complete factory
+            'user_id' => null,
+            'terms' => $this->faker->randomElement([3, 6]),
+            'amount' => $this->faker->numberBetween(1000, 10000),
+            'outstanding_amount' => function($attributes) {
+                return $attributes['amount'];
+            },
+            'currency_code' => $this->faker->randomElement(['SGD', 'VND']),
+            'processed_at' => $this->faker->date(),
+            'status' => Loan::STATUS_DUE
         ];
     }
 }
